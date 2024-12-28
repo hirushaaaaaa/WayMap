b2e78a01dfa98729fe50db8da4e339f55cd11744
sithijagimhan <sithijagimhan19@gmail.com>
2024-12-29 01:14:58 +0530

    update

--- app/src/main/java/com/example/waymap/homeActivity.java
+++ app/src/main/java/com/example/waymap/homeActivity.java
@@ -3,18 +3,19 @@ package com.example.waymap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class homeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Find the Distance Calculator LinearLayout
-        LinearLayout distanceCalculatorLayout = findViewById(R.id.distanceCalculator);
+        // Find the Distance Calculator LinearLayout (Correct ID)
+        ConstraintLayout distanceCalculatorLayout = findViewById(R.id.distanceCalculator);
 
         // Find the ImageView and TextView
         ImageView distanceCalculatorImage = findViewById(R.id.imageButton);
